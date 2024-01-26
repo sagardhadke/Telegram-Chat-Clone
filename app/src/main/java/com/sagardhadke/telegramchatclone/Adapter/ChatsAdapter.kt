4b37f91350f649d58a1e6329c6849497b84c2f75
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-26 23:43:18 +0530

    Added Chats Data

--- app/src/main/java/com/sagardhadke/telegramchatclone/Adapter/ChatsAdapter.kt
+++ app/src/main/java/com/sagardhadke/telegramchatclone/Adapter/ChatsAdapter.kt
@@ -1,4 +1,30 @@
 package com.sagardhadke.telegramchatclone.Adapter
 
-class ChatsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sagardhadke.telegramchatclone.Model.ChatsModel
+import com.sagardhadke.telegramchatclone.databinding.DummyChatsBinding
+
+class ChatsAdapter (val context: Context,val chatsArrayList:ArrayList<ChatsModel>):RecyclerView.Adapter<ChatsAdapter.ViewHolder>(){
+
+    inner class ViewHolder(val binding: DummyChatsBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = DummyChatsBinding.inflate(LayoutInflater.from(context),parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return chatsArrayList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.teleImg.setImageResource(chatsArrayList[position].image)
+        holder.binding.teleChatName.text =chatsArrayList[position].name
+        holder.binding.teleDiss.text = chatsArrayList[position].lastmessage
+        holder.binding.teleDate.text = chatsArrayList[position].time
+    }
+
 }
--- app/src/main/java/com/sagardhadke/telegramchatclone/MainActivity.kt
+++ app/src/main/java/com/sagardhadke/telegramchatclone/MainActivity.kt
@@ -2,10 +2,47 @@ package com.sagardhadke.telegramchatclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sagardhadke.telegramchatclone.Adapter.ChatsAdapter
+import com.sagardhadke.telegramchatclone.Model.ChatsModel
+import com.sagardhadke.telegramchatclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit  var chatsAdapter : ChatsAdapter
+    lateinit var chatsArrayList: ArrayList<ChatsModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        chatsArrayList = ArrayList()
+        chatsArrayList.add(ChatsModel(R.drawable.chat1,"Unique Computer","Hey Welcom to the Unique Computer","Jan 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat2,"Cake Shop","Your cake is ready come and collect your cake","Fed 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat3,"Joy","Hello I am using Telegram","Jun 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat4,"Priya","Hey bro, Priya Here","May 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat5,"Unique Computer","Hey Welcom to the Unique Computer","Apr 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats6,"Nasa","Testing Chats","Nov 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats7,"Zee5","subscribe Sagar Dhadke Youtube Channel","Dec 26"))
+
+        chatsArrayList.add(ChatsModel(R.drawable.chat2,"Cake Shop","Your cake is ready come and collect your cake","Fed 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat3,"Joy","Hello I am using Telegram","Jun 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat4,"Priya","Hey bro, Priya Here","May 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat5,"Unique Computer","Hey Welcom to the Unique Computer","Apr 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats6,"Nasa","Testing Chats","Nov 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats7,"Zee5","subscribe Sagar Dhadke Youtube Channel","Dec 26"))
+
+        chatsArrayList.add(ChatsModel(R.drawable.chat2,"Cake Shop","Your cake is ready come and collect your cake","Fed 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat3,"Joy","Hello I am using Telegram","Jun 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat4,"Priya","Hey bro, Priya Here","May 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chat5,"Unique Computer","Hey Welcom to the Unique Computer","Apr 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats6,"Nasa","Testing Chats","Nov 26"))
+        chatsArrayList.add(ChatsModel(R.drawable.chats7,"Zee5","subscribe Sagar Dhadke Youtube Channel","Dec 26"))
+
+        chatsAdapter = ChatsAdapter(this,chatsArrayList)
+        binding.chatsData.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.chatsData.adapter = chatsAdapter
+
     }
 }
